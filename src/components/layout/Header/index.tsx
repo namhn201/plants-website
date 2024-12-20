aaea28251443603da88dd41553ac333d06549b3b
namhn201 <namhn201@gmail.com>
2024-12-20 13:14:52 +0700

    chỉnh header

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -247,47 +247,6 @@ const Header = () => {
             </ListItem>
           </Link>
 
-          {/* <Accordion
-            open={open === 2}
-            icon={
-              <ChevronDownIcon
-                strokeWidth={2.5}
-                className={`mx-auto h-4 w-4 transition-transform ${
-                  open === 2 ? "rotate-180" : ""
-                }`}
-              />
-            }
-          >
-            <ListItem className="p-0" selected={open === 2}>
-              <AccordionHeader
-                onClick={() => handleOpen(2)}
-                className="border-b-0 p-3"
-              >
-                <ListItemPrefix>
-                  <ShoppingBagIcon className="h-5 w-5" />
-                </ListItemPrefix>
-                <Typography color="blue-gray" className="mr-auto font-normal">
-                  Phụ kiện
-                </Typography>
-              </AccordionHeader>
-            </ListItem>
-            <AccordionBody className="py-1">
-              <List className="p-0">
-                <ListItem>
-                  <ListItemPrefix>
-                    <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
-                  </ListItemPrefix>
-                  Orders
-                </ListItem>
-                <ListItem>
-                  <ListItemPrefix>
-                    <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
-                  </ListItemPrefix>
-                  Products
-                </ListItem>
-              </List>
-            </AccordionBody>
-          </Accordion> */}
           <Link href={routerName.Team}>
             <ListItem>
               <ListItemPrefix>
@@ -361,7 +320,7 @@ const Header = () => {
   return (
     <div
     className={`transition-all duration-500 ${
-      showHeader ? "bg-[#3E7160]" : "bg-transparent"
+      showHeader ? "bg-[#3E7160] shadow-lg" : "bg-transparent"
     } fixed top-0 left-0 w-full z-40`}
     id="header"
   >
